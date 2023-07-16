db1ed3755bbe8b2dda235f6b9ced3a3b8f6255e0
Mihir Patki <mihirpatki80@gmail.com>
2023-07-16 16:25:38 +0530

    sutraction inserted

--- Sum.java
+++ Sum.java
@@ -7,7 +7,8 @@ public static void main(String args[]){
 
     int a,b;
 
-    System.out.println("Addition is : "+(a+b);
+    System.out.println("Addition is : "+(a+b));
+    System.out.println("Subtraction is : "+(a-b));
 
 
 
